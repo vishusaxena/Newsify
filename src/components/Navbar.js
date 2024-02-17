f921ac1316267f524488434182056988a8d9ee0c
vishusaxena <vishusaxena3105@gmail.com>
2024-02-17 22:04:03 +0530

    API integarted

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg bg-light ">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">Newsify</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,10 +13,10 @@ export default class Navbar extends Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                <a className="nav-link active" aria-current="page" href="/home">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">Link</a>
+                <a className="nav-link" href="/categories">Categories</a>
               </li>
               
              
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -7,7 +7,7 @@ export default class Newsitem extends Component {
         return (
             <div className='my-3'>
                 <div className="card" style={{ width: "18rem" }}>
-                    <img src={imageUrl} className="card-img-top" alt="..."  />
+                    <img src={imageUrl?imageUrl:"https://lh3.googleusercontent.com/J6_coFbogxhRI9iM864NL_liGXvsQp2AupsKei7z0cNNfDvGUmWUy20nuUhkREQyrpY4bEeIBuc=s0-w300-rw"} className="card-img-top" alt="..."  />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
