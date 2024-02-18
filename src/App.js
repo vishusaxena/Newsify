2bd38d4ce587e1ef40ce755c65495fe0bbb4bc51
vishusaxena <vishusaxena3105@gmail.com>
2024-02-18 19:12:39 +0530

    added categories

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News'
+import { Routes, Route } from "react-router-dom"
 
 
 export default class App extends Component {
   render() {
     return (
       <>
-      <Navbar/>
-     
-      <News pageSize={12} country={"in"} category={"sports"}/>
+<Navbar />
+        <Routes>
+          
+          <Route exact path="/general"  element={<News key="general" pageSize={12} country={"in"} category={"general"} />
+          } />
+          <Route exact path="/business"  element={<News key="business" pageSize={12} country={"in"} category={"business"} />
+          } />
+          <Route exact path="/entertainment" element={<News key="entertainment"  pageSize={12} country={"in"} category={"entertainment"} />
+          } />
+          <Route exact path="/health"  element={<News key="health" pageSize={12} country={"in"} category={"health"} />
+          } />
+          <Route exact path="/science"  element={<News key="science" pageSize={12} country={"in"} category={"science"} />
+          } />
+          <Route exact path="/sports"  element={<News key="sports" pageSize={12} country={"in"} category={"sports"} />
+          } />
+          <Route exact path="/technology" element={<News  key="technology" pageSize={12} country={"in"} category={"technology"} />
+          } />
+          
+        </Routes>
       </>
     )
   }
